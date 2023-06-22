bf33de4908321647ac7c71d50cd2836f6fa04e7b
JiriOndrusek <ondrusek.jiri@gmail.com>
2023-06-22 16:20:44 +0200

    POC testing

--- recipes/src/main/java/org/apache/camel/quarkus/update/ExtendedContextRecipe.java
+++ recipes/src/main/java/org/apache/camel/quarkus/update/ExtendedContextRecipe.java
@@ -1,11 +1,15 @@
 package org.apache.camel.quarkus.update;
 
 import org.openrewrite.ExecutionContext;
+import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.TreeVisitor;
+import org.openrewrite.java.AddImport;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.MethodMatcher;
+import org.openrewrite.java.RemoveImport;
+import org.openrewrite.java.RemoveUnusedImports;
 import org.openrewrite.java.tree.Expression;
 import org.openrewrite.java.tree.J;
 import org.openrewrite.java.tree.JavaType;
@@ -16,38 +20,88 @@ public class ExtendedContextRecipe extends Recipe {
     private static final MethodMatcher MATCHER_CONTEXT_GET_EXT =
             new MethodMatcher("org.apache.camel.CamelContext getExtension(java.lang.Class)");
     private static final MethodMatcher MATCHER_GET_NAME_RESOLVER =
-            new MethodMatcher("org.apache.camel.ExtendedCamelContext getComponentNameResolver");
+            new MethodMatcher("org.apache.camel.ExtendedCamelContext getComponentNameResolver()");
+
+    private static final MethodMatcher MATCHER_CONTEXT_GET_EXT_RUNTIME_CATALOG =
+            new MethodMatcher("org.apache.camel.CamelContext getExtension(java.lang.Class)");
 
     @Override
     public String getDisplayName() {
-        return "Replaces removed method involving ExternalCamelContext.";
+        return "Replaces removed method involving ExternalCamelContext";
     }
 
     @Override
     public String getDescription() {
-        return "Changes method call `context.getExtension(ExtendedCamelContext.class).getComponentNameResolver()` to a call of static method `PluginHelper.getComponentNameResolver(context)`";
+        return "Changes method call `context.getExtension(ExtendedCamelContext.class).getComponentNameResolver()` to a call of static method `PluginHelper.getComponentNameResolver(context)`.";
     }
 
     @Override
     protected TreeVisitor<?, ExecutionContext> getVisitor() {
         return new JavaIsoVisitor<>() {
 
+            private static boolean pluginHelperImportRequired;
+
+//            @Override
+//            public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext executionContext) {
+//                if(new MethodMatcher("org.apache.camel.quarkus.component.test.it.Test test").matches(method.getMethodType())) {
+//                    System.out.println("");
+//                }
+//
+//                return super.visitMethodDeclaration(method, executionContext);
+//            }
+
+
+//            @Override
+//            public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext executionContext) {
+//                return super.visitVariableDeclarations(multiVariable, executionContext);
+//            }
+
+//            @Override
+//            public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+//
+//                if (new MethodMatcher("org.apache.camel.ExtendedCamelContext getComponentNameResolver").matches((J.MethodInvocation) method)) {
+//                    System.out.println(method);
+//                }
+//                return super.visitMethodInvocation(method, executionContext);
+//            }
+
             @Override
             public Expression visitExpression(Expression expression, ExecutionContext executionContext) {
                 Expression e = super.visitExpression(expression, executionContext);
+
+                //todo move to visitMethodInvocation
+
+                if (new MethodMatcher("org.apache.camel.quarkus.component.test.it.Test test").matches(e)) {
+                    System.out.println("");
+                }
+                // context.getExtension(ExtendedCamelContext.class).getComponentNameResolver() -> PluginHelper.getComponentNameResolver(context)
                 if (MATCHER_GET_NAME_RESOLVER.matches(e)) {
                     J.MethodInvocation mi = (J.MethodInvocation) e;
                     if (mi.getSelect() instanceof J.MethodInvocation && MATCHER_CONTEXT_GET_EXT.matches(((J.MethodInvocation) mi.getSelect()).getMethodType())) {
                         J.MethodInvocation innerInvocation = (J.MethodInvocation) mi.getSelect();
                         e = e.withTemplate(JavaTemplate.builder(() -> getCursor().getParentOrThrow(), "PluginHelper.getComponentNameResolver(#{any(org.apache.camel.CamelContext)})")
                                         //todo import does not work ??
-                                        .imports("org.apache.camel.support.PluginHelper")
                                         .build(),
                                 e.getCoordinates().replace(), innerInvocation.getSelect());
+
+                        pluginHelperImportRequired = true;
+
+
+//                        doAfterVisit(new AddImport<>("org.apache.camel.support.PluginHelper", null, false));
+//                        doAfterVisit(new RemoveImport("org.apache.camel.ExtendedCamelContext"));
+                        doAfterVisit(new AddImport<>("org.apache.camel.support.PluginHelper", null, false));
+//                        doAfterVisit(new RemoveUnusedImports());
                     }
+                }
+
+
+                //context.getExtension(RuntimeCamelCatalog.class) -> context.getCamelContextExtension().getContextPlugin(RuntimeCamelCatalog.class);
+                if (MATCHER_CONTEXT_GET_EXT_RUNTIME_CATALOG.matches(e)) {
+                    J.MethodInvocation mi = (J.MethodInvocation) e;
                 }
                 return e;
             }
+
         };
     }
 }
---
+++ recipes/src/test/java/org/apache/camel/quarkus/update/ExtendedContextRecipeTest.java
@@ -0,0 +1,63 @@
+package org.apache.camel.quarkus.update;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.test.RewriteTest.toRecipe;
+
+public class ExtendedContextRecipeTest implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipe(new ExtendedContextRecipe())
+                .parser(JavaParser.fromJavaVersion()
+                        .logCompilationWarningsAndErrors(true)
+                        .classpath("camel-api"));
+    }
+
+    @Test
+    void test() {
+        rewriteRun(
+                spec -> spec.recipe(toRecipe(() -> new ExtendedContextRecipe().getVisitor())),
+                java(
+                        """
+                                package org.apache.camel.quarkus.component.test.it;
+                                
+                                import org.apache.camel.CamelContext;
+                                import org.apache.camel.ExtendedCamelContext;
+                                import org.apache.camel.spi.ComponentNameResolver;
+                                
+                                public class Test {
+                                
+                                    CamelContext context;
+                                
+                                    public void test() {
+                                        ComponentNameResolver ec = context.getExtension(ExtendedCamelContext.class).getComponentNameResolver();
+                                    }
+                                }
+                            """,
+                        """
+                                package org.apache.camel.quarkus.component.test.it;
+                                
+                                import org.apache.camel.CamelContext;
+                                import org.apache.camel.ExtendedCamelContext;
+                                import org.apache.camel.spi.ComponentNameResolver;
+                                import org.apache.camel.support.PluginHelper;
+                                
+                                public class Test {
+                                
+                                    CamelContext context;
+                                
+                                    public void test() {
+                                        ComponentNameResolver ec = PluginHelper.getComponentNameResolver(context);
+                                    }
+                                }
+                                """
+                )
+        );
+    }
+}
+
