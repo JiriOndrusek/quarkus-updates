09d0fa7bb4fedc4a5384f2eeeac707e4c7d0b702
JiriOndrusek <ondrusek.jiri@gmail.com>
2024-08-27 16:41:21 +0200

    should work

--- recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate42Test.java
+++ recipes-tests/src/test/java/io/quarkus/updates/camel/CamelUpdate42Test.java
@@ -10,4 +10,13 @@ import org.openrewrite.test.TypeValidation;
 import static org.openrewrite.java.Assertions.java;
 
 public class CamelUpdate42Test extends org.apache.camel.updates.camel44.CamelUpdate42Test {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        //initialize parser
+        super.defaults(spec);
+        //use Quarkus recipe
+        spec.recipe(getClass().getResourceAsStream("/quarkus-updates/org.apache.camel.quarkus/camel-quarkus/3.8.yaml"),
+                "io.quarkus.updates.camel.camel44.CamelQuarkusMigrationRecipe");
+    }
 }
